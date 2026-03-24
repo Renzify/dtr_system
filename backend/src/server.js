6d8e7ec145c0d2c752dde645f1d8a5b650ce57c5
Renzify <168178279+Renzify@users.noreply.github.com>
2026-03-24 10:27:59 +0800

    setup express backend & app deployment

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import path from "path";
+import cors from "cors";
+import { ENV } from "./lib/env.js";
+
+const app = express();
+
+const __dirname = path.resolve();
+
+const { PORT, CLIENT_URL, NODE_ENV } = ENV;
+
+app.use(cors({ origin: CLIENT_URL, credentials: true }));
+
+if (NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+app.listen(PORT, () => {
+  console.log("Server is running on port:", PORT);
+});
